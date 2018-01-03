86e9739e24f92a9a25e97a65a3174fa0d13a5610
Lanre Kadiri <omkadiri8@gmail.com>
2018-01-02 21:56:25 -0500

    adding url

--- src/markerclusterer.js
+++ src/markerclusterer.js
@@ -198,7 +198,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @type {string}
  * @private
  */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../images/m';
+MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = "https://raw.githubusercontent.com/omkadiri/google-markerclusterer/master/images/m";
 
 
 /**
