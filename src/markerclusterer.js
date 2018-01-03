c4415039769e8916914aa7ff4365e8e1429b376a
Lanre Kadiri <omkadiri8@gmail.com>
2018-01-02 22:39:19 -0500

    adding google image url

--- src/markerclusterer.js
+++ src/markerclusterer.js
@@ -198,7 +198,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @type {string}
  * @private
  */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = "https://raw.githubusercontent.com/omkadiri/google-markerclusterer/master/images/m";
+MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = "https://raw.githubusercontent.com/googlemaps/v3-utility-library/master/markerclusterer/images/m";
 
 
 /**
